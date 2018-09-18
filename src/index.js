4e12402e1854f4a19cefcb79a202d33010359e38
Nekrashevich, Natalya <n.nekrashevich@a1qa.com>
2018-09-18 14:27:30 +0300

    comments deleted

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 class Sorter {
   constructor() {
     this.array = [];
-    this.comparatorFunction =/* (a, b)=> (a - b);*/    function(a, b){return a>b?true:false;}
+    this.comparatorFunction = function(a, b){
+      return a > b ? true : false;
+    }
   }
 
   add(element) {
@@ -41,19 +43,17 @@ class Sorter {
       return this.array;
     }
     indices.sort();
+    
     var bufArray = [];  
     indices.forEach(item => {
       bufArray.push(this.array[item]);
     });
         
     bubbleSort(bufArray, this.comparatorFunction);
-    //console.log(bufArray);  
     indices.forEach((item, i) => {
-      //console.log(item+"  "+i+ "  "+this.array[item]+"   "+bufArray[i]);
-      this.array[item]=bufArray[i];
+      this.array[item] = bufArray[i];
     })
     bufArray.length = 0;
-    //console.log(bufArray);
   }
 
   setComparator(compareFunction) {
